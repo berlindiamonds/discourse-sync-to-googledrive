6d896d6baf31cbcd123483ff07afe83f63a3a796
KAJATIGER <ksantro@gmx.de>
2017-07-12 16:58:59 +0200

    test

--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -2,12 +2,10 @@ module DiscourseBackupToDrive
 	class DriveSynchronizer
 		CHUNK_SIZE = 25600000
 		UPLOAD_MAX_SIZE = CHUNK_SIZE * 4
-		
+		byebug
 		def self.sync
 
 			session = GoogleDrive::Session.from_service_account_key("#{Dir.pwd}/plugins/discourse-googledrive-backup/lib/config.json")
-			google = GoogleService.new
-			google.create_service(compute, scopes, session)
 			
 
 			folder_name = Discourse.current_hostname
