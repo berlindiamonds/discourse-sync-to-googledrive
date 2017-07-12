f44b1595eb20ab49eb1bb260ada7cc3ce2901a81
Jen <jvarelalijo@gmail.com>
2017-07-12 10:50:07 +0200

    improve current uploader

--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -2,24 +2,24 @@ module DiscourseBackupToDrive
   class DriveSynchronizer
 
     def self.sync
-
       session = GoogleDrive::Session.from_config("config.json")
       folder_name = Discourse.current_hostname
 
-      local_backup_files = Backup.all.map(&:filename).take(2)
+      local_backup_files = Backup.all.map(&:filename).take(SiteSetting.discourse_backups_to_drive_quantity)
 
       local_backup_files.each do |filename|
         full_path = Backup[filename].path
         file = session.upload_from_file(full_path, filename)
-        found = session.collection_by_title("#{folder_name}")
-        if found
-          found.add(file)
+        folder = session.collection_by_title(folder_name)
+        if folder.present?
+          folder.add(file)
         else
-          folder = session.root_collection.create_subcollection("#{folder_name}")
+          folder = session.root_collection.create_subcollection(folder_name)
           folder.add(file)
         end
         session.root_collection.remove(file)
       end
     end
+    
   end
 end
