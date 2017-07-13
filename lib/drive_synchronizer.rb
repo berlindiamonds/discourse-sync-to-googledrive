8bcd6cbb42cddce4a29f593a640065e2dbcfef2b
Jen <jvarelalijo@gmail.com>
2017-07-13 11:43:20 +0200

    session & service_session

--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -1,27 +1,26 @@
 module DiscourseBackupToDrive
+  class DriveSynchronizer
 
-	class DriveSynchronizer
-		CHUNK_SIZE = 25600000
-		UPLOAD_MAX_SIZE = CHUNK_SIZE * 4
-		
-		def self.sync
+    def self.sync
+      session = GoogleDrive::Session.from_config("config.json")
+      service_session = GoogleDrive::Session.from_service_account_key("servicekey.json")
 
-			session = GoogleDrive::Session.from_service_account_key("#{Dir.pwd}/plugins/discourse-googledrive-backup/lib/config.json")
+      folder_name = Discourse.current_hostname
 
-			folder_name = Discourse.current_hostname
+      local_backup_files = Backup.all.map(&:filename).take(SiteSetting.discourse_backups_to_drive_quantity)
 
-	     	local_backup_files.each do |filename|
-	        full_path = Backup[filename].path
-	        file = session.upload_from_file(full_path, filename)
-	        folder = session.collection_by_title(folder_name)
-	        if folder.present?
-	          folder.add(file)
-	        else
-	          folder = session.root_collection.create_subcollection(folder_name)
-	          folder.add(file)
-	        end
-	        session.root_collection.remove(file)
-	    end
+      local_backup_files.each do |filename|
+        full_path = Backup[filename].path
+        file = session.upload_from_file(full_path, filename)
+        folder = session.collection_by_title(folder_name)
+        if folder.present?
+          folder.add(file)
+        else
+          folder = session.root_collection.create_subcollection(folder_name)
+          folder.add(file)
+        end
+        session.root_collection.remove(file)
+      end
     end
 
   end
