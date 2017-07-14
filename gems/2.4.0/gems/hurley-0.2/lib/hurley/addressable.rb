e9d49c5327340774dd41ef5a96edaca6158e3662
Jen <jvarelalijo@gmail.com>
2017-07-14 18:48:03 +0200

    Remove gems directory

--- gems/2.4.0/gems/hurley-0.2/lib/hurley/addressable.rb
+++
@@ -1,9 +0,0 @@
-# Enables Addressable::URI support in Hurley
-
-require "addressable/uri"
-
-module Hurley
-  class Url
-    @@parser = Addressable::URI.method(:parse)
-  end
-end
--- gems/2.4.0/gems/retriable-3.0.2/lib/retriable/version.rb
+++
@@ -1,3 +0,0 @@
-module Retriable
-  VERSION = "3.0.2".freeze
-end
