7480dc9b80b9e03799432c46bd83b38741b809e5
KAJATIGER <ksantro@gmx.de>
2017-07-21 13:41:48 +0200

    base class for the synchronizer

---
+++ synchronizer.rb
@@ -0,0 +1,25 @@
+class Synchronizer
+
+	attr_reader :backup
+
+	def initialize
+		@backup = backup
+    @api_key = SiteSetting.discourse_backups_to_drive_api_key
+    @turned_on = SiteSetting.discourse_backups_to_drive_enabled
+	end
+
+	def can_sync?
+    @turned_on && @api_key.present? && backup.present?
+  end
+
+  def sync
+    if @backup.can_sync
+      @backup.perform_sync
+    end
+  end
+
+  protected
+  def perform_sync
+  end
+
+end
