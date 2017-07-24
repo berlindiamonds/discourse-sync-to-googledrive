8a235b07793e96492c3dcf992e2f9f26a41161f3
Jen <jvarelalijo@gmail.com>
2017-07-24 12:12:53 +0200

    break drive sync into methods

--- app/jobs/regular/sync_backups_to_drive.rb
+++ app/jobs/regular/sync_backups_to_drive.rb
@@ -4,7 +4,7 @@ module Jobs
     sidekiq_options queue: 'low'
 
     def execute(args)
-      local_backup_files = Backup.all.map(&:filename).take(SiteSetting.discourse_backups_to_drive_quantity)
+      local_backup_files = Backup.all.map(&:filename).take(SiteSetting.discourse_backups_quantity)
       local_backup_files each do |backup|
         ::DiscourseBackupToDrive::DriveSynchronizer.new(backup).sync
       end
--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -3,20 +3,28 @@ module DiscourseBackupToDrive
 
     protected
     def perform_sync
-      session = GoogleDrive::Session.from_service_account_key(StringIO.new(@api_key))
-      folder_name = Discourse.current_hostname
-      local_backup_files = Backup.all.map(&:filename).take(@number_of_backups)
-      local_backup_files.each do |filename|
-        full_path = Backup[filename].path
-        file = session.upload_from_file(full_path, filename)
-        folder = session.collection_by_title(folder_name)
-        if folder.present?
-          folder.add(file)
-        else
-          folder = session.root_collection.create_subcollection(folder_name)
-          folder.add(file)
-        end
+      session = session_meth
+      # local_backup_files = Backup.all.map(&:filename).take(SiteSetting.discourse_backups_quantity)
+      local_backup_files.each do |f|
+        full_path = Backup[f].path
+        file = session.upload_from_file(full_path, f)
+        add_to_folder(session, file)
         session.root_collection.remove(file)
+      end
+    end
+
+    def self.session_meth
+      GoogleDrive::Session.from_service_account_key(StringIO.new(@api_key))
+    end
+
+    def add_to_folder(session, file)
+      folder_name = Discourse.current_hostname
+      folder = session.collection_by_title(folder_name)
+      if folder.present?
+        folder.add(file)
+      else
+        folder = session.root_collection.create_subcollection(folder_name)
+        folder.add(file)
       end
     end
 
