4f5972c38c6c8e9636e8d5f9d9dfc50943969c14
KAJATIGER <ksantro@gmx.de>
2017-07-25 15:37:50 +0200

    eviltrout suggested to put the initialize method into the drive synchronizer

--- lib/synchronizer.rb
+++ lib/synchronizer.rb
@@ -2,25 +2,12 @@ class Synchronizer
 
   attr_reader :backup
 
-  def backup
-    @backup
-  end
-
   def initialize(backup)
     @backup = backup
-    @api_key = SiteSetting.discourse_backups_api_key
-    @turned_on = SiteSetting.discourse_backups_enabled
-    @number_of_backups = SiteSetting.discourse_backups_quantity
-  end
-
-  def can_sync?
-    @turned_on && @api_key.present? && backup.present?
   end
 
   def sync
-    if can_sync?
-      perform_sync
-    end
+    perform_sync  if can_sync?
   end
 
   protected
