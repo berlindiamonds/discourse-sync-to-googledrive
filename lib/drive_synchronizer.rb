61427ca8225b3609b116ea073b00a5001bdb2b77
Jen <jvarelalijo@gmail.com>
2017-07-25 13:42:10 +0200

    drive synchronizer (#23)
    * break drive sync into methods
    
    * sync one backup at a time
    
    * fix backup quantity
    
    * add session initializer and call parent constructor

--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -1,24 +1,29 @@
 module DiscourseBackupToDrive
   class DriveSynchronizer < Synchronizer
 
-    protected
-    def perform_sync
-      session = GoogleDrive::Session.from_service_account_key(StringIO.new(@api_key))
-      folder_name = Discourse.current_hostname
-      local_backup_files = Backup.all.map(&:filename).take(@number_of_backups)
-      local_backup_files.each do |filename|
-        full_path = Backup[filename].path
-        file = session.upload_from_file(full_path, filename)
-        folder = session.collection_by_title(folder_name)
-        if folder.present?
-          folder.add(file)
-        else
-          folder = session.root_collection.create_subcollection(folder_name)
-          folder.add(file)
-        end
-        session.root_collection.remove(file)
-      end
+    def initialize(backup)
+      super(backup)
+      @session = GoogleDrive::Session.from_service_account_key(StringIO.new(@api_key))
     end
 
+    protected
+    def perform_sync
+      full_path = backup.path
+      filename = backup.filename
+      file = @session.upload_from_file(full_path, filename)
+      add_to_folder(@session, file)
+      @session.root_collection.remove(file)
+    end
+
+    def add_to_folder(session, file)
+      folder_name = Discourse.current_hostname
+      folder = session.collection_by_title(folder_name)
+      if folder.present?
+        folder.add(file)
+      else
+        folder = session.root_collection.create_subcollection(folder_name)
+        folder.add(file)
+      end
+    end
   end
 end
--- lib/synchronizer.rb
+++ lib/synchronizer.rb
@@ -2,12 +2,16 @@ class Synchronizer
 
   attr_reader :backup
 
-	def initialize(backup)
-    @backup             = backup
-    @api_key            = SiteSetting.discourse_backups_api_key
-    @turned_on          = SiteSetting.discourse_backups_enabled
-    @number_of_backups  = SiteSetting.discourse_backups_quantity
-	end
+  def backup
+    @backup
+  end
+
+  def initialize(backup)
+    @backup = backup
+    @api_key = SiteSetting.discourse_backups_api_key
+    @turned_on = SiteSetting.discourse_backups_enabled
+    @number_of_backups = SiteSetting.discourse_backups_quantity
+  end
 
   def can_sync?
     @turned_on && @api_key.present? && backup.present?
