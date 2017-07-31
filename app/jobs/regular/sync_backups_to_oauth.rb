ef1dbd89bdbdc8abcc1e48fb1668809801e3e2ce
KAJATIGER <ksantro@gmx.de>
2017-07-31 15:43:35 +0200

    broke the line into blocks

--- app/jobs/regular/sync_backups_to_oauth.rb
+++ app/jobs/regular/sync_backups_to_oauth.rb
@@ -4,7 +4,9 @@ module Jobs
     sidekiq_options queue: 'low'
 
     def execute(arg)
-      Backup.all.take(SiteSetting.discourse_backups_drive_quantity).each {|backup| DiscourseBackupToDrive::OauthSynchronizer.new(backup).sync }
+      Backup.all.take(SiteSetting.discourse_sync_to_googledrive_quantity).each do
+        |backup| DiscourseBackupToDrive::OauthSynchronizer.new(backup).sync
+      end
     end
   end
 end
