28123b3cbc9cf976e3ac2a85a7a6a18cc15ad2b8
KAJATIGER <ksantro@gmx.de>
2017-08-09 10:27:26 +0200

    this works in every case, but needs to be refactored. I added the conditions unless google_files == nil, do the unique upload. if they are not nil they still do the upload and add to folder, but the code is there 2 times and it looks ugly and repetitve. also I added the condition unless the google folder is nil remove old files, cause if there is no folder, the session.collection bla bla is nil as well and the method doesn't know what to do. All this can be done in a prettier way

--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -5,7 +5,6 @@ module DiscourseBackupToDrive
       super(backup)
       @api_key = SiteSetting.discourse_sync_to_googledrive_api_key
       @turned_on = SiteSetting.discourse_sync_to_googledrive_enabled
-      @folder_name = Discourse.current_hostname
     end
 
     def session
@@ -23,34 +22,45 @@ module DiscourseBackupToDrive
     end
 
     def upload_unique_files
-      ([backup] - session.collection_by_title(@folder_name).files).each do |f|
-        if f.present?
-          full_path = backup.path
-          filename = backup.filename
-          file = session.upload_from_file(full_path, filename)
-          add_to_folder(file)
-          session.root_collection.remove(file)
+      folder_name = Discourse.current_hostname
+      unless session.collection_by_title(folder_name) == nil
+        ([backup] - session.collection_by_title(folder_name).files).each do |f|
+          if f.present?
+            full_path = backup.path
+            filename = backup.filename
+            file = session.upload_from_file(full_path, filename)
+            add_to_folder(file)
+            session.root_collection.remove(file)
+          end
         end
       end
+      full_path = backup.path
+      filename = backup.filename
+      file = session.upload_from_file(full_path, filename)
+      add_to_folder(file)
+      session.root_collection.remove(file)
     end
 
     def add_to_folder(file)
-      @folder_name = Discourse.current_hostname
-      folder = session.collection_by_title(@folder_name)
+      folder_name = Discourse.current_hostname
+      folder = session.collection_by_title(folder_name)
       if folder.present?
         folder.add(file)
       else
-        folder = session.root_collection.create_subcollection(@folder_name)
+        folder = session.root_collection.create_subcollection(folder_name)
         folder.add(file)
       end
     end
 
     def remove_old_files
-      google_files = session.collection_by_title(@folder_name).files
-      sorted = google_files.sort_by {|x| x.created_time}
-      keep = sorted.take(SiteSetting.discourse_sync_to_googledrive_quantity)
-      trash = google_files - keep
-      trash.each { |d| d.delete(true) }
+      folder_name = Discourse.current_hostname
+      unless session.collection_by_title(folder_name) == nil
+        google_files = session.collection_by_title(folder_name).files
+        sorted = google_files.sort_by {|x| x.created_time}
+        keep = sorted.take(SiteSetting.discourse_sync_to_googledrive_quantity)
+        trash = google_files - keep
+        trash.each { |d| d.delete(true) }
+      end
     end
   end
 end
