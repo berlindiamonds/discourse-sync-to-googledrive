ca7f2c25e336b514be62ec2ad407f13ca7729193
KAJATIGER <ksantro@gmx.de>
2017-08-09 11:10:38 +0200

    refactored but I still need to try if it works

--- lib/drive_synchronizer.rb
+++ lib/drive_synchronizer.rb
@@ -17,32 +17,28 @@ module DiscourseBackupToDrive
 
     protected
     def perform_sync
-      upload_unique_files
-      remove_old_files
-    end
-
-    def upload_unique_files
       folder_name = Discourse.current_hostname
-      unless session.collection_by_title(folder_name) == nil
-        ([backup] - session.collection_by_title(folder_name).files).each do |f|
-          if f.present?
-            full_path = backup.path
-            filename = backup.filename
-            file = session.upload_from_file(full_path, filename)
-            add_to_folder(file)
-            session.root_collection.remove(file)
-          end
-        end
-      end
       full_path = backup.path
       filename = backup.filename
       file = session.upload_from_file(full_path, filename)
-      add_to_folder(file)
-      session.root_collection.remove(file)
+      unless session.collection_by_title(folder_name) == nil
+        upload_unique_files(file, folder_name)
+        remove_old_files(folder_name)
+      else
+        add_to_folder(file, folder_name)
+      end
     end
 
-    def add_to_folder(file)
-      folder_name = Discourse.current_hostname
+    def upload_unique_files(file, folder_name)
+      ([backup] - session.collection_by_title(folder_name).files).each do |f|
+        if f.present?
+          add_to_folder(file, folder_name)
+        end
+      end
+      add_to_folder(file, folder_name)
+    end
+
+    def add_to_folder(file, folder_name)
       folder = session.collection_by_title(folder_name)
       if folder.present?
         folder.add(file)
@@ -50,17 +46,15 @@ module DiscourseBackupToDrive
         folder = session.root_collection.create_subcollection(folder_name)
         folder.add(file)
       end
+      session.root_collection.remove(file)
     end
 
-    def remove_old_files
-      folder_name = Discourse.current_hostname
-      unless session.collection_by_title(folder_name) == nil
-        google_files = session.collection_by_title(folder_name).files
-        sorted = google_files.sort_by {|x| x.created_time}
-        keep = sorted.take(SiteSetting.discourse_sync_to_googledrive_quantity)
-        trash = google_files - keep
-        trash.each { |d| d.delete(true) }
-      end
+    def remove_old_files(folder_name)
+      google_files = session.collection_by_title(folder_name).files
+      sorted = google_files.sort_by {|x| x.created_time}
+      keep = sorted.take(SiteSetting.discourse_sync_to_googledrive_quantity)
+      trash = google_files - keep
+      trash.each { |d| d.delete(true) }
     end
   end
 end
