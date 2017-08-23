c13311f9ede636547b67e0c182c3041f0d9f8802
Jen <jvarelalijo@gmail.com>
2017-08-23 21:42:30 +0200

    use nil to initialise DriveDownloader

--- app/controllers/downloaders_controller.rb
+++ app/controllers/downloaders_controller.rb
@@ -2,8 +2,7 @@ class DownloadersController < ApplicationController
   skip_before_filter :check_xhr
 
   def show
-    backups = Backup.all.take(SiteSetting.discourse_sync_to_googledrive_quantity)
-    google_list = DiscourseDownloadFromDrive::DriveDownloader.new(backups).list_files_json
+    google_list = DiscourseDownloadFromDrive::DriveDownloader.new(nil).list_files_json
 
     respond_to do |format|
       format.json {render json: google_list}
