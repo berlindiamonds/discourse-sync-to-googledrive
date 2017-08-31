4c2c037dfbbb3b2e2570e602301aaa0f188d2ae9
Jen <jvarelalijo@gmail.com>
2017-08-31 08:05:15 +0200

    closed spec blocks

--- spec/downloaders_controller_spec.rb
+++ spec/downloaders_controller_spec.rb
@@ -34,30 +34,30 @@ describe ApplicationController::DownloadersController, type: :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+  end
 
-    describe "POST #create" do
-      let(:sample_file_id) {
-        "0B7WjYjWZJv_4blA0a2p6RzVraFE"
-      }
+  describe "POST #create" do
+    let(:sample_file_id) {
+      "0B7WjYjWZJv_4blA0a2p6RzVraFE"
+    }
 
 
-      before {
-        drive_instance = DiscourseDownloadFromDrive::DriveDownloader
-        drive_instance.any_instance.stubs(:file_id).returns(sample_file_id)
-      }
+    before {
+      drive_instance = DiscourseDownloadFromDrive::DriveDownloader
+      drive_instance.any_instance.stubs(:file_id).returns(sample_file_id)
+    }
 
-      it "sends a google-file-id to the job" do
-        xhr :post, :create
-        @file_id = :sample_file_id
-        expect(@file_id).to eq(:sample_file_id)
-      end
-
-      it "responds with 200 status" do
-        xhr :post, :create
-        expect(response).to be_success
-        expect(response).to have_http_status(200)
-      end
-
+    it "sends a google-file-id to the job" do
+      xhr :post, :create
+      @file_id = :sample_file_id
+      expect(@file_id).to eq(:sample_file_id)
     end
+
+    it "responds with 200 status" do
+      xhr :post, :create
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+    
   end
 end
