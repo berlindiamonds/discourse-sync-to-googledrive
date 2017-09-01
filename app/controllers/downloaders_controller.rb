9112dfd26150095305c279417bf3a797c38a6d2a
Robin Ward <robin.ward@gmail.com>
2017-09-01 14:32:38 -0400

    Clean up controller before adding ember routes

--- app/controllers/downloaders_controller.rb
+++ app/controllers/downloaders_controller.rb
@@ -1,24 +1,16 @@
 require "email_backup_token"
 
-class DownloadersController < ApplicationController
-  skip_before_filter :check_xhr
-  # skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+class DownloadersController < Admin::AdminController
+  requires_plugin 'discourse-sync-to-googledrive'
 
   def index
     google_list = DiscourseDownloadFromDrive::DriveDownloader.new(nil).json_list
-
-    respond_to do |format|
-      format.json {render json: google_list}
-      format.html {render html: google_list}
-    end
+    render json: google_list
   end
 
   def create
     @file_id = params[:file_id]
     Jobs.enqueue(:send_download_drive_link, file_id: @file_id)
-    respond_to do |format|
-      format.json {render json: @file_id}
-      format.html {render html: @file_id}
-    end
+    render json: { file_id: @file_id }
   end
 end
