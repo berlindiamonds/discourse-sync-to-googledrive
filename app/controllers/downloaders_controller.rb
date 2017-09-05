fa6c454206c5473ba8733115df8e4c8cdc009377
KAJATIGER <ksantro@gmx.de>
2017-09-05 09:52:29 +0200

    in order to get the test running green, I had to store the json list into an instance variable, because otherwise I got a parsin error

--- app/controllers/downloaders_controller.rb
+++ app/controllers/downloaders_controller.rb
@@ -1,17 +1,15 @@
 require "email_backup_token"
 
 class DownloadersController < Admin::AdminController
-  skip_before_filter :check_xhr
-  # skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
   def index
-    google_list = DiscourseDownloadFromDrive::DriveDownloader.new(nil).json_list
-    render json: google_list
+    @google_list = DiscourseDownloadFromDrive::DriveDownloader.new(nil).json_list
+    render json: @google_list
   end
 
   def create
     @file_id = params[:file_id]
     Jobs.enqueue(:send_download_drive_link, file_id: @file_id)
-    render json: google_list
+    render json: @google_list
   end
 end
