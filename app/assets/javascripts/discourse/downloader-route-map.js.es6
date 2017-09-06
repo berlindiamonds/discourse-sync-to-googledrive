5bcd994984641634443a63bb08237e54dfb531df
Jen <jvarelalijo@gmail.com>
2017-09-06 08:36:06 +0200

    delete assets inside app/ and fix controller

--- app/assets/javascripts/discourse/downloader-route-map.js.es6
+++
@@ -1,9 +0,0 @@
-export default {
-  resource: 'admin.adminPlugins',
-  map() {
-    // We want `discourse-sync-to-googledrive/downloader` in this case
-    this.route('discourse-sync-to-googledrive', { resetNamespace: true }, function() {
-      this.route('downloader');
-    });
-  }
-};
--- app/controllers/downloaders_controller.rb
+++ app/controllers/downloaders_controller.rb
@@ -1,27 +1,16 @@
 require "email_backup_token"
 
 class DownloadersController < Admin::AdminController
-<<<<<<< HEAD
-
-  def index
-    @google_list = DiscourseDownloadFromDrive::DriveDownloader.new(nil).json_list
-    render json: @google_list
-=======
   requires_plugin 'discourse-sync-to-googledrive'
 
   def index
     google_list = DiscourseDownloadFromDrive::DriveDownloader.new(nil).json_list
     render json: google_list
->>>>>>> 1de56de0d793f1a5a3baf1a4f7f487b0a4c6c640
   end
 
   def create
     @file_id = params[:file_id]
     Jobs.enqueue(:send_download_drive_link, file_id: @file_id)
-<<<<<<< HEAD
-    render json: @google_list
-=======
     render json: { file_id: @file_id }
->>>>>>> 1de56de0d793f1a5a3baf1a4f7f487b0a4c6c640
   end
 end
