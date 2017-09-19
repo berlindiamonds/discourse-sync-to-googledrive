7d5c7f56a76d892b1d4cd2a369529932073b3478
KAJATIGER <ksantro@gmx.de>
2017-09-19 10:32:34 +0200

    getting the file path in the #create action from the downloader

--- app/controllers/downloaders_controller.rb
+++ app/controllers/downloaders_controller.rb
@@ -11,6 +11,7 @@ class DownloadersController < Admin::AdminController
 
   def create
     file_id = params.fetch(:file_id)
+    file_path = DiscourseDownloadFromDrive::DriveDownloader.new(file_id).download
     download_url = "#{url_for(controller: 'downloaders', action: 'create')}"
     Jobs.enqueue(:send_download_drive_link, to_address: 'example@email.com', drive_url: download_url)
     render nothing: true
