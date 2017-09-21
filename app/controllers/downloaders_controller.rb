ed2ea217c0daca48a5ae795a886c894b10d0c576
KAJATIGER <ksantro@gmx.de>
2017-09-21 14:07:42 +0200

    added the token to the url and also a few for the expired link

--- app/controllers/downloaders_controller.rb
+++ app/controllers/downloaders_controller.rb
@@ -12,16 +12,29 @@ class DownloadersController < Admin::AdminController
 
   def create
     file_id = params.fetch(:file_id)
+    token = EmailBackupToken.set(current_user.id)
     file_path = DiscourseDownloadFromDrive::DriveDownloader.new(file_id).download
-    download_url = "#{url_for(controller: 'downloaders', action: 'show')}"
+    download_url = "#{url_for(controller: 'downloaders', action: 'show')}?token=#{token}"
     Jobs.enqueue(:send_download_drive_link, to_address: 'teamberlindiamonds@gmail.com', drive_url: download_url)
     render nothing: true
   end
 
   def show
-    file_id = params.fetch(:file_id)
-    filename = DiscourseDownloadFromDrive::DriveDownloader.new(file_id).filename
-    file_path = File.join(Backup.base_directory, filename)
-    send_file file_path
+    if !EmailBackupToken.compare(current_user.id, params.fetch(:token))
+      @error = I18n.t('download_backup_mailer.no_token')
+    end
+    if !@error
+      file_id = params.fetch(:file_id)
+      filename = DiscourseDownloadFromDrive::DriveDownloader.new(file_id).filename
+      file_path = File.join(Backup.base_directory, filename)
+      EmailBackupToken.del(current_user.id)
+      send_file file_path
+    else
+      if @error
+        render layout: 'no_ember', status: 422, text: "this link has already been used and is therefore expired"
+      else
+        render nothing: true, status: 404
+      end
+    end
   end
 end
